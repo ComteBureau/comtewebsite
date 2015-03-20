6fdd20ea9b25d62b220676f31dff92559b532bc7
Thomas Viktil <mandarin@mandarin.no>
2015-03-20 11:01:49 +0100

    Bare minimum

---
+++ main.js
@@ -0,0 +1,14 @@
+// var routehandlers   = require('./app/routehandlers');
+// var config          = require('./config.json');
+// // var website         = require('./lib/index');
+// var website         = require('prismic-website');
+
+// website.on('ready', function(webserver) {
+//     routehandlers.init(webserver);
+// });
+
+// website.init(config, {
+//     base: __dirname
+// });
+
+console.log('Hello and goodbye');
