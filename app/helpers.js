093ffe02d4a37bd09941f5b27ba980be1901199f
Thomas Viktil <mandarin@mandarin.no>
2015-04-01 15:56:46 +0200

    Colors moved to css, upgraded prismic-website
    prismic-website 0.1.1 returns null when there are no images to show. Makes it possible to use Handlebars' if helper without checking a property on photo.

--- app/helpers.js
+++ app/helpers.js
@@ -1,27 +1,5 @@
 var Handlebars = require('handlebars');
 
-module.exports.color = function(context, options) {
-    var colors = {
-        'Green':    '#6CE0A2',
-        'Blue':     '#82CDFF',
-        'Orange':   '#FFE494',
-        'Teal':     '#64D8DE',
-        'Salmon':   '#fa8072',
-        'Gray':     '#cccccc',
-        'White':    '#ffffff'
-    };
-
-    var col;
-    if (typeof context === 'undefined' ||
-        typeof colors[context] === 'undefined') {
-        col = colors.White;
-    } else {
-        col = colors[context];
-    }
-
-    return new Handlebars.SafeString(col);
-}
-
 module.exports.lowercase = function(context, options) {
     return new Handlebars.SafeString(context.toLowerCase());
 }
