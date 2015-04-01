2cdabdfbc01db39689e126eb8b92fd0c85358105
Thomas Viktil <mandarin@mandarin.no>
2015-04-01 15:31:26 +0200

    Basic markup OK

--- app/helpers.js
+++ app/helpers.js
@@ -25,3 +25,32 @@ module.exports.color = function(context, options) {
 module.exports.lowercase = function(context, options) {
     return new Handlebars.SafeString(context.toLowerCase());
 }
+
+module.exports.imagestyle = function(name, photo) {
+    var style = "<style type='text/css'>"+
+
+        "#section_"+name+" {"+
+            "background-image: url("+photo.main+");"+
+        "}"+
+
+        "@media only screen and (max-width: 1440px) {"+
+            "#section_"+name+" {"+
+                "background-image: url("+photo.large+");"+
+            "}"+
+        "}"+
+
+        "@media only screen and (max-width: 720px) {"+
+            "#section_"+name+" {"+
+                "background-image: url("+photo.medium+");"+
+            "}"+
+        "}"+
+
+        "@media only screen and (max-width: 375px) {"+
+            "#section_"+name+" {"+
+                "background-image: url("+photo.small+");"+
+            "}"+
+        "}"+
+
+    "</style>";
+    return new Handlebars.SafeString(style);
+}
